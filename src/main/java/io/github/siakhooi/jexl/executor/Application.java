2cf73fcc80e3014a0ebe9157e51f940511c00380
Siak Hooi <siakhooi@gmail.com>
2026-07-04 08:24:57 +0000

    exit 0 on no arguments

--- src/main/java/io/github/siakhooi/jexl/executor/Application.java
+++ src/main/java/io/github/siakhooi/jexl/executor/Application.java
@@ -3,10 +3,18 @@ package io.github.siakhooi.jexl.executor;
 import picocli.CommandLine;
 
 public class Application {
-    public static void main(String[] args) {
+
+    public static int run(String[] args) {
         CommandLine cmd = new CommandLine(new ApplicationCommandLine());
         cmd.setUsageHelpWidth(120);
-        int exitCode = cmd.execute(args);
-        System.exit(exitCode);
+        if (args.length == 0) {
+            cmd.usage(System.out);
+            return 0;
+        }
+        return cmd.execute(args);
+    }
+
+    public static void main(String[] args) {
+        System.exit(run(args));
     }
 }
